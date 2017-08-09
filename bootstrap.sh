1724406548abc8c2c67a03b75c3307640c6797d7
Anton Dorofeev <anton@dorofeev.me>
2017-08-09 11:26:49 +0000

    improved script behaviour when running multiple times

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 SSH_KEYS="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/6CW7KXHRJzpHB1dH5oioyNgfiAQEgldYtl4WKQjSWzodz0kBAmA/R3vV4S2cwt52V1FwlBtZdZCQT8ffUiB/lkawp/D0gHlYwFQCR/hI0yb8t4MktaP4YsyGrLLyQRxBQsClenzxW7v69P5Gargd4WygxdAAibbizg2V1vlIp3NRqchA0/lXh7eknHY3LMUtTc2UYkRTfKmyWpHnYZs6rPtSoL02uKrJvi9Un2kkW3mRqxtHXUlrl7gf5YexzLBUHnxLtiJTb97ZPxXHftcwscjyKmopNUSwMRqBCvUK57bM2jLL6gzxLumZ1zZZxi21/88UI9DaLXZgm5vmJT1p anton@dell500"
 if [ $(cat ~/.ssh/authorized_keys | grep "$SSH_KEYS" | wc -l) -eq 0 ] ; then
-    ssh github.com -o "StrictHostKeyChecking no"
+    ssh github.com -o "StrictHostKeyChecking no" || true
     echo "$SSH_KEYS" >> ~/.ssh/authorized_keys
     chmod 600 ~/.ssh/authorized_keys
 fi
@@ -28,14 +28,16 @@ if id "user" >/dev/null 2>&1; then
     echo "user exists"
 else
     adduser --disabled-password --gecos "" user
-    if [ $(cat ~user/.ssh/authorized_keys | grep "$SSH_KEYS" | wc -l) -eq 0 ] ; then
-        su - user -c 'ssh github.com -o "StrictHostKeyChecking no"'
-        echo "$SSH_KEYS" >> ~user/.ssh/authorized_keys
-        chmod 600 ~user/.ssh/authorized_keys
-        PRIVATE_SSH_KEY_FILE="~user/.ssh/id_rsa"
-        if [ ! -f "$PRIVATE_SSH_KEY_FILE" ] ; then
-            ssh-keygen -t rsa -N '' -f "$PRIVATE_SSH_KEY_FILE"
-        fi
-    fi
+fi
+if [ $(cat ~user/.ssh/authorized_keys | grep "$SSH_KEYS" | wc -l) -eq 0 ] ; then
+    su - user -c 'ssh github.com -o "StrictHostKeyChecking no" || true'
+    echo "$SSH_KEYS" >> ~user/.ssh/authorized_keys
+    chmod 600 ~user/.ssh/authorized_keys
+    chown user. ~user/.ssh/authorized_keys
+fi
+PRIVATE_SSH_KEY_FILE="~user/.ssh/id_rsa"
+if [ ! -f "$PRIVATE_SSH_KEY_FILE" ] ; then
+    su - user -c 'ssh github.com -o "StrictHostKeyChecking no" || true'
+    su - user -c "ssh-keygen -t rsa -N '' -f $PRIVATE_SSH_KEY_FILE"
 fi
 
