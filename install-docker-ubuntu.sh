0b5d6f6b36f0c2273619904b23c87de30c76665e
Anton Dorofeev <dos@null-squad.com>
2017-10-22 13:12:34 +0300

    [draft] ubuntu docker installer

---
+++ install-docker-ubuntu.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+
+apt-get install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install -y docker-ce=17.09.0~ce-0~ubuntu
+
+usermod -aG docker user
+
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
