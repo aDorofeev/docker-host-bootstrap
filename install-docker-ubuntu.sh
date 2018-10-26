7da504c68c75fcb35b94f3d441281855c72bed42
Anton Dorofeev <anton@dorofeev.me>
2018-10-26 16:21:10 +0200

    use latest docker and compose

--- install-docker-ubuntu.sh
+++ install-docker-ubuntu.sh
@@ -18,7 +18,9 @@ add-apt-repository \
 
 apt-get update
 
-apt-get install -y docker-ce=17.12.0~ce-0~ubuntu
+LATEST_DOCKER=$(apt-cache madison docker-ce | sort -r | head -n 1 | awk '{print $3}')
+
+apt-get install -y docker-ce="${LATEST_DOCKER}"
 
 apt-mark hold docker-ce
 
@@ -26,4 +28,13 @@ usermod -aG docker user
 
 curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# get latest docker compose released tag
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+# Install docker-compose
+sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose.tm["
+chmod +x /usr/local/bin/docker-compose.tmp
+mv /usr/local/bin/docker-compose.tmp /usr/local/bin/docker-compose
+sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
