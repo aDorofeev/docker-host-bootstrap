25a381aaac034530fdbee69d883180f47bbfff16
Anton Dorofeev <anton@dorofeev.me>
2019-01-31 21:27:46 +0200

    [bugfix] typo

--- install-docker-ubuntu.sh
+++ install-docker-ubuntu.sh
@@ -33,7 +33,7 @@ chmod +x /usr/local/bin/docker-compose
 COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 
 # Install docker-compose
-sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose.tm["
+sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose.tmp"
 chmod +x /usr/local/bin/docker-compose.tmp
 mv /usr/local/bin/docker-compose.tmp /usr/local/bin/docker-compose
 sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
